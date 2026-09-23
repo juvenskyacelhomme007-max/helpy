543f36b07d21283da090042e6697d8ece648bbee
juvenskyacelhomme007-max <juvenskyacelhomme007@gmail.com>
2026-09-23 16:36:53 -0500

    Delete api.js

--- backend/api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getHome, getHealth } = require("./controllers");
-
-const router = express.Router();
-
-router.get("/", getHome);
-router.get("/health", getHealth);
-
-module.exports = router;
